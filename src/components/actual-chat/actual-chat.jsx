b9c115bb5d55cb9171e6f1a67d3ee92ae3672bb9
jhon.guzman <alex903g@gmail.com>
2023-05-06 17:39:13 -0500

    Messages panel

--- src/components/actual-chat/actual-chat.jsx
+++ src/components/actual-chat/actual-chat.jsx
@@ -13,8 +13,75 @@ export const ChatPanel = () => {
                 <img src="https://img.freepik.com/vector-premium/perfil-avatar-hombre-icono-redondo_24640-14044.jpg?w=826" alt="Avatar"/>
                 <p>Text</p>
             </section>
-            <section className='chat-panel-messages'>
-                dd
+            <section className="chat-panel-container">
+                <ul className='chat-panel-messages'>
+                    <li className='chat-panel-received message-box'>
+                        Estakjsdahdjkashdjksa dhsakjds
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Estakjsdahdjkashdjksa dhsakjds
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Estakjsdahdjkashdjksa dhsakjds
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Estakjsdahdjkashdjksa dhsakjds
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Estakjsdahdjkashdjksa dhsakjds
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Recibido
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Recibido
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Recibido
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Recibido
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Recibido
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Recibido
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-sent message-box'>
+                        Enviado
+                    </li>
+                    <li className='chat-panel-received message-box'>
+                        Recibido
+                    </li>
+                </ul>
             </section>
             <section className='chat-panel-textbox'>
                 <FontAwesomeIcon className='icon' icon={faFaceSmileWink} />
