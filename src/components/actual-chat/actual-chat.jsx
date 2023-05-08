11290fed95c5b9c1f0dc878803eb1095b7eb8545
jhon.guzman <alex903g@gmail.com>
2023-05-07 23:52:54 -0500

    scrollbar css

--- src/components/actual-chat/actual-chat.jsx
+++ src/components/actual-chat/actual-chat.jsx
@@ -13,7 +13,6 @@ export const ChatPanel = () => {
                 <img src="https://img.freepik.com/vector-premium/perfil-avatar-hombre-icono-redondo_24640-14044.jpg?w=826" alt="Avatar"/>
                 <p>Text</p>
             </section>
-
             <section className='chat-panel-messages-container'>
                 <ul className='chat-panel-messages'>
                     <li className='chat-panel-sent message-box'>
@@ -100,7 +99,7 @@ export const ChatPanel = () => {
                 <FontAwesomeIcon className='icon' icon={faFaceSmileWink} />
                 <div className='chat-panel-textbox-container'>
                     <div className='chat-panel-textbox-group'>
-                        <div id="chat-panel-input-text" contentEditable />
+                        <div id="chat-panel-input-text" contentEditable onInput={e => setActualMessage(e.target.innerText)} />
                         <div className='chat-panel-placeholder'>Type a message</div>
                     </div>
                 </div>
